c4fdf402d06c0bc61b9245fcadd02a8507455643
Himanshu <codeforinterviewwithhimanshu@gmail.com>
2024-07-13 19:27:37 +0530

    Use parenthesis

--- Practice Folders/Practice 4/4.c
+++ Practice Folders/Practice 4/4.c
@@ -9,7 +9,7 @@ int main() {
     int sum = 0;
 
     for (int i = 1; i < 11; i++)
-        sum += 8*i;
+        sum += (8*i);
     
     printf("The sum of the numbers occuring in the multiplication table of 8 is %d.\n", sum);
     
