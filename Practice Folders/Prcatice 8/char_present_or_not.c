68aea95465e513b2cbc2d60d005fb8972c37e5b9
Himanshu <codeforinterviewwithhimanshu@gmail.com>
2024-07-15 19:23:31 +0530

    Finish last question of Project 8 Strings

--- Practice Folders/Prcatice 8/char_present_or_not.c
+++ Practice Folders/Prcatice 8/char_present_or_not.c
@@ -2,17 +2,31 @@
 Write a program to check whether a given character is present in a string or not.
 */
 #include<stdio.h>
+#include<string.h>
 
-int presentOrNot(char str[]) {
-    return 1;
+int presentOrNot(char str[], char character) {
+    for(int i = 0; i < strlen(str); i++)
+        if (str[i] == character)
+            return 1;
+
+    return 0;
 }
 
 int main() {
-    char a;
-    printf("Enter charcter: ");
-    scanf("%c", &a);
-    printf("%c", a);
-    
 
+    char str[20];
+    char character;
+
+    printf("Enter the string without leaving any spaces: ");
+    scanf("%s", str);
+
+    printf("Enter the character you want to check: ");
+    scanf(" %c", &character);
+
+    if (presentOrNot(str, character)) 
+        printf("%c is present in %s", character, str);
+    else 
+        printf("%c is not present in %s", character, str);
+    
     return 0;
 }
